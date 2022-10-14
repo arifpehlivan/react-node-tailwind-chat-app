ab82d30526b1268cf520cec46f47035593c71580
arifpehlivan <arifpehlivanapap@gmail.com>
2022-10-14 14:23:32 +0300

    add socket

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-const Room = () => {
+const Room = ({username,setUsername,room,setRoom,setChatScreen,socket}) => {
+    const sendRoom = () => {
+        socket.emit("room", room);
+        setChatScreen(true);
+    }
     return (
         <div className='flex items-center justify-center h-full'>
             <div className='w-1/3 h-[320px] rounded-lg bg-indigo-600 flex flex-col space-y-4 p-3'>
                 <h1 className='text-center my-4 font-bold text-2xl'>Welcome To Chat</h1>
-                <input className='h-12 rounded-xl p-3 outline-none' type="text" placeholder='Username'/>
-                <input className='h-12 rounded-xl p-3 outline-none' type="text" placeholder='Room'/>
-                <div className='hover:opacity-70 bg-indigo-900 text-white cursor-pointer h-12 pt-2 text-xl text-center rounded-xl'>CHAT!!!</div>
+                <input value={username} onChange={e => setUsername(e.target.value)} className='h-12 rounded-xl p-3 outline-none' type="text" placeholder='Username'/>
+                <input value={room} onChange={e => setRoom(e.target.value)} className='h-12 rounded-xl p-3 outline-none' type="text" placeholder='Room'/>
+                <div onClick={sendRoom} className='hover:opacity-70 bg-indigo-900 text-white cursor-pointer h-12 pt-2 text-xl text-center rounded-xl'>CHAT!!!</div>
             </div>
         </div>
     )
